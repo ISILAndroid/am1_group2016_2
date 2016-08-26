85aa3c7e135e7836dda0d3830f1ae70deda886d5
Eduardo José Medina Alfaro <emedinaa@gmail.com>
2016-08-25 21:05:47 -0500

    Update form.java

--- form.java
+++ form.java
@@ -15,13 +15,25 @@ public class HelloWorld
 		
    	String nombre="Eduardo";
     String email ="abc@abc.com";
+    String password1= "123";
+    String password2= "123";
+    
     boolean state= validarTxt(nombre);
     boolean stateEmail= isEmailValid(email);
+    boolean statePassword= compararCampos(password1,password2);
+
     
     System.out.print("	validar nombre "+state);
-     System.out.print("	validar email "+stateEmail);
+    System.out.print("	validar email "+stateEmail);
+    System.out.print("	validar password "+statePassword);
   }
   
+  private static boolean compararCampos(String txt1, String txt2){
+    if(!txt1.equals(txt2)){
+      return false;
+    }
+    return true;
+  }
   private static boolean validarTxt(String txt){
     if(txt.isEmpty())
     {
