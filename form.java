61ed12cce3d72bc5af055d3920fae4caf8694258
Eduardo José Medina Alfaro <emedinaa@gmail.com>
2016-08-25 21:01:12 -0500

    Update form.java

--- form.java
+++ form.java
@@ -4,6 +4,8 @@
 //*******************************************************************
 
 import java.lang.Math; // header stuff MUST go above the first class
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // our main class becomes a file but the main method is still found
 public class HelloWorld
@@ -11,10 +13,13 @@ public class HelloWorld
   public static void main(String[] args)
   {
 		
-   	String nombre="";
+   	String nombre="Eduardo";
+    String email ="abc@abc.com";
     boolean state= validarTxt(nombre);
+    boolean stateEmail= isEmailValid(email);
     
-    System.out.print("	validar "+state);
+    System.out.print("	validar nombre "+state);
+     System.out.print("	validar email "+stateEmail);
   }
   
   private static boolean validarTxt(String txt){
@@ -24,5 +29,27 @@ public class HelloWorld
     }
     return true;
   }
+  
+   private static boolean isEmailValid(String email) {
+        String regExpn =
+                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                        +"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                        +"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                        +"([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+
+        CharSequence inputStr = email;
+
+        Pattern pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+
+        if(matcher.matches())
+            return true;
+        else
+            return false;
+    }
+  
+  
 }
 
