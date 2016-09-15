4cc7ec089f51e459b20ca5224b2b8f3aff9519c6
Eduardo Medina <emedinaa@gmail.com>
2016-09-15 15:52:03 -0500

    layout samples

---
+++ LayoutSamples/app/src/main/java/com/isil/layoutsamples/MainActivity.java
@@ -0,0 +1,20 @@
+package com.isil.layoutsamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_linear);
+        //setContentView(R.layout.layout_linear_horizontal);
+        //setContentView(R.layout.layout_login);
+        //setContentView(R.layout.layout_peso);
+        //setContentView(R.layout.layout_pesov);
+        //setContentView(R.layout.layout_relative1);
+        //setContentView(R.layout.layout_relative_example);
+    }
+}
