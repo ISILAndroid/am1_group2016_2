6956ef9c7527a7f107437537194e86b824d0647e
Eduardo José Medina Alfaro <emedinaa@gmail.com>
2016-09-22 18:58:38 -0500

    added Layout examples

---
+++ LayoutExamples/app/src/main/java/com/isil/exampleslayout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.isil.exampleslayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.layout_login);
+        //setContentView(R.layout.activity_linear);
+    }
+}
