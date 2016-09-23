9ec0b92f9ed9541470afa51dcbd4cc982230a27f
Eduardo Medina <emedinaa@gmail.com>
2016-09-22 21:05:37 -0500

    example 5

--- LayoutExamples/app/src/main/java/com/isil/exampleslayout/MainActivity.java
+++ LayoutExamples/app/src/main/java/com/isil/exampleslayout/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_layout);
+        setContentView(R.layout.layout_example5);
         //setContentView(R.layout.layout_login);
         //setContentView(R.layout.activity_linear);
     }
