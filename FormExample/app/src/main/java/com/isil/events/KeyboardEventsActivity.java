4c7364cf180a6c4344b42fad7b537589cbfe7c1f
Eduardo Medina <emedinaa@gmail.com>
2016-10-06 19:10:45 -0500

    add examples of user events

---
+++ FormExample/app/src/main/java/com/isil/events/KeyboardEventsActivity.java
@@ -0,0 +1,103 @@
+package com.isil.events;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class KeyboardEventsActivity extends Activity {
+
+    private EditText eteUsername;
+    private EditText eteEmail;
+    private EditText etePassword;
+    private Button btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keyboard_events);
+        ui();
+    }
+
+    private void ui() {
+        eteUsername= (EditText)findViewById(R.id.eteUsername);
+        eteEmail= (EditText)findViewById(R.id.eteEmail);
+        etePassword= (EditText)findViewById(R.id.etePassword);
+        btnSignUp= (Button) findViewById(R.id.btnSignUp);
+
+        //events
+        eteUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        eteEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        etePassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    send();
+                }
+                return false;
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyboard();
+                send();
+            }
+        });
+    }
+
+    private void hideKeyboard() {
+        ((InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE))
+                .toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0);
+    }
+
+    private void showKeyboard() {
+        ((InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE))
+                .toggleSoftInput(0, InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    private void send() {
+        Toast.makeText(this, "Send server... ",Toast.LENGTH_SHORT).show();
+    }
+}
